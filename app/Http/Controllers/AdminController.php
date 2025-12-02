26bf93ee8e5085773da429127578c357c9cb3992
Gelana3225 <gelogeti@gmail.com>
2025-12-02 11:23:08 -0800

    Today's updates: room module, admin pages, migrations

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Room;
+
 class AdminController extends Controller
 {
     public function index()
@@ -35,5 +37,39 @@ class AdminController extends Controller
     public function home()
     {
         return view('home.index');
+    }
+
+    public function create_room()
+    {
+        return view('admin.create_room');
+    }
+
+    public function add_room(Request $request)
+    {
+        $data = new Room;
+
+        $data->room_title = $request->title;
+
+        $data->description = $request->description;
+
+        $data->room_price = $request->price;
+
+        $data->wifi = $request->wifi;
+
+        $data->room_type = $request->type;
+
+        $image=$request->image;
+        if($image)
+        {
+            $imagename=time().'.'.$image->getClientOriginalExtension();
+            $request->image->move('room',$imagename);
+
+            $data->image=$imagename;
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
     }
 }
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -77,9 +77,7 @@
                         </x-slot>
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+
 
                             <x-dropdown-link href="{{ route('profile.show') }}">
                                 {{ __('Profile') }}
